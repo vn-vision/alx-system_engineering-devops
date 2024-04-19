2cd46b7ec886581826a2622fe6093813e617ca8d
vn-vision <vctrngang@gmail.com>
2024-04-19 21:43:44 +0300

    First 'P'uppet owner

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -9,14 +9,14 @@ command => '/usr/bin/apt-get update'
 # make sure that the directory exists with set owner and permission
 file { $file:
 ensure => 'directory',
-owner  => 'www-data',
-group  => 'www-data',
-mode   => '0744'
 }
 
 # create file school in directory /tmp
 file { "${file}/school":
 ensure  => 'present',
 content => 'I love Puppet',
+owner   => 'www-data',
+group   => 'www-data',
+mode    => '0744'
 require => File[$file]
 }
