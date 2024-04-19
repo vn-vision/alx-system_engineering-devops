9bb5a4f10ddadcea6e992b8279bdda0ddcdd8c62
vn-vision <vctrngang@gmail.com>
2024-04-19 21:47:10 +0300

    First 'P'uppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,18 +1,10 @@
-# create a file in /tmp
-
-$file='/tmp'
-
-# make sure that the directory exists with set owner and permission
-file { $file:
-ensure => 'directory',
-}
-
 # create file school in directory /tmp
-file { "${file}/school":
+
+
+file { '/tmp/school':
 ensure  => 'present',
 content => 'I love Puppet',
 owner   => 'www-data',
 group   => 'www-data',
 mode    => '0744'
-require => File[$file]
 }
