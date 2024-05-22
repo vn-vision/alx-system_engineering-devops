3df96fe2303549718c8986e67cef7839a4644387
kZilla <kzilla@vn-z.zilla>
2024-05-22 09:26:53 +0300

    Dump them files

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' export data to CSV file given the ID '''
+
+import csv
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    # limit importation
+    userId = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(userId)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": userId}).json()
+
+    with open("{}.csv".format(userId), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [userId, username, t.get("completed"), t.get("title")]
+            ) for t in todos]
---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' export data to JSON file given the ID '''
+
+import sys
+import json
+import requests
+
+
+if __name__ == "__main__":
+    # limit importation
+    userId = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(userId)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": userId}).json()
+
+    with open("{}.json".format(userId), "w") as jsonfile:
+        json.dump({userId: [{"task": t.get("title"), "completed": t.get("completed"), "username": username} for t in todos]}, jsonfile)
